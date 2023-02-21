24c19542cf73f826ef680759de87eba74dd227cc
andreu <andreu@beta-robots.com>
2023-02-21 19:21:04 +0100

    Defines DetectorBase class

---
+++ include/detector_base.h
@@ -0,0 +1,38 @@
+#ifndef TARGET_DETECTOR_DETECTOR_BASE_H
+#define TARGET_DETECTOR_DETECTOR_BASE_H
+
+//std
+#include <iostream>
+#include <map>
+
+//Eigen
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Geometry>
+
+namespace TargetDetector
+{
+
+class DetectorBase
+{
+	protected:
+
+	public:
+		DetectorBase();
+		~DetectorBase();
+
+		// initialization with custom parameters, depending on each class specialization
+		virtual bool init(const std::map<std::string, std::string> & __params) = 0;
+
+		// clears input sensory data stored in class members, to start a new detection
+		virtual void resetData();
+
+		// detect method only defines output arguments. Input data depending on the class specialization
+		virtual bool detect(
+			std::vector<Eigen::Vector3d> & __positions,
+			std::vector<Eigen::Quaterniond> & __orientations,
+		 	std::vector<double> & __confidences) = 0;
+};
+
+} //namespace
+
+#endif
